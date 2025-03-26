17acd32fc35aa717e3fd50c5db6b8f53b340f33c
Umer Mushtaq <umer.mushtaq@u-paris2.fr>
2025-03-26 19:27:40 +0100

    cli vars, input reads scripts

---
+++ input_rd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\n *** welcome to the input reading script *** \n"
+
+
+echo -e "\n * what's your name friend? * \n"
+
+read uname
+
+echo -e "\n hey $uname, it's very nice to meet you! \n"
