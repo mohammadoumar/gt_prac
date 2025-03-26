4d3e4cb56401400d6b70dd9a2f94c3270bff3deb
Umer Mushtaq <umer.mushtaq@u-paris2.fr>
2025-03-26 15:57:13 +0100

    edit try, make addition bash

---
+++ addition.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#name="alice"
+
+echo "welcome to the addition script"
+
+#a=5
+#b=10
+
+c=$(($1+$2))
+
+echo "the sum of the two numbers you entered is: $1 + $2 = $c"
+
+#if [ $1 -lt 100 ] || [ $1 -eq 100 ]
+#then
+#   echo "the number you entered ($1) is less than or equal to 100"
+#else
+#   echo "the number you entered ($1) is greater than 100"
+#fi
--- try_script.sh
+++ try_script.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 echo "hello"
-#name="alice"
-#echo "the person's name is $name"
 
-#a=5
-#b=10
-
-#c=$((a+b))
-
-#if [ $1 -lt 100 ] || [ $1 -eq 100 ]
-#then
-#    echo "the number you entered ($1) is less than or equal to 100"
-#else
-#    echo "the number you entered ($1) is greater than 100"
-#fi
 
 if [ $1 -gt 10 ]
 then
