75c4d4fb7bc5a6e115d5d6f6121b45e33a10f7f2
Umer Mushtaq <umer.mushtaq@u-paris2.fr>
2025-03-26 16:11:31 +0100

    sanity check in addition script

--- addition.sh
+++ addition.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#name="alice"
 
 echo "welcome to the addition script"
 
-#a=5
-#b=10
-
-c=$(($1+$2))
-
-echo "the sum of the two numbers you entered is: $1 + $2 = $c"
-
+if [ -z $1 ] || [ -z $2 ]
+then
+    echo "you did not enter any arguments, no addition possible. run script again"
+else
+    c=$(($1+$2))
+    echo "the sum of the two numbers you entered is: $1 + $2 = $c"
+fi
 #if [ $1 -lt 100 ] || [ $1 -eq 100 ]
 #then
 #   echo "the number you entered ($1) is less than or equal to 100"
